5da7c6ebad5ce84e2fb518b1e6c652bf20d1b110
schmalz <schmalz@live.co.uk>
2019-09-15 07:50:21 +0100

    Randomly-placed triangles.

--- src/play_quil_static_process/dynamic.clj
+++ src/play_quil_static_process/dynamic.clj
@@ -2,21 +2,32 @@
   (:require [clojure.pprint :as pp]
             [quil.core :as q]))
 
-(defn- save-file-name []
+(defn- save-file-name
+  []
   (pp/cl-format nil
                 "~d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-~2,'0d.tif"
                 (q/year) (q/month) (q/day) (q/hour) (q/minute) (q/seconds)))
 
-(defn setup []
+(defn setup
+  []
   (q/color-mode :hsb 360 100 100 1.0))
 
-(defn draw []
+(defn- paint-triangle
+  "Paint a triangle."
+  [x y alpha offset]
+  (q/fill 68 100 60 alpha)
+  (q/triangle (- x offset) y x y x (+ y offset)))
+
+(defn- paint-triangle-set
+  "Paint a congruent set of three triangles."
+  [x y]
+  (dorun (map #(paint-triangle x y %1 %2) [0.25 0.5 0.75] [200 150 100])))
+
+(defn draw
+  []
   (q/no-loop)
-  (q/no-fill)
+  (q/no-stroke)
   (q/background 44 10 99)
-  (q/stroke 68 100 60)
-  (dotimes [n 88]
-    (let [x (* 10 (inc n))
-          y x]
-      (q/triangle x y x (+ y 10) (+ x 10) y)))
+  (dotimes [_ 10]
+    (paint-triangle-set (q/random (q/width)) (q/random (q/height))))
   (q/save (save-file-name)))
