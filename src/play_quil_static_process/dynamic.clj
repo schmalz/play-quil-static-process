d81012ab1398750e2c1c3767d34dc6c10c431a3c
schmalz <schmalz@live.co.uk>
2019-09-16 14:14:27 +0100

    Try a different colour and background.

--- src/play_quil_static_process/dynamic.clj
+++ src/play_quil_static_process/dynamic.clj
@@ -15,19 +15,20 @@
 (defn- paint-triangle
   "Paint a triangle."
   [x y alpha offset]
-  (q/fill 68 100 60 alpha)
+  ;(q/fill 356 98.3 46.1 alpha)
+  (q/fill 337 97 60.4 alpha)
   (q/triangle (- x offset) y x y x (+ y offset)))
 
 (defn- paint-triangle-set
   "Paint a congruent set of three triangles."
   [x y]
-  (dorun (map #(paint-triangle x y %1 %2) [0.25 0.5 0.75] [200 150 100])))
+  (dorun (map #(paint-triangle x y %1 %2) [0.25 0.35 0.45] [200 150 100])))
 
 (defn draw
   []
   (q/no-loop)
   (q/no-stroke)
-  (q/background 44 10 99)
+  (q/background 0 0 95)
   (dotimes [_ 10]
     (paint-triangle-set (q/random (q/width)) (q/random (q/height))))
   (q/save (save-file-name)))
